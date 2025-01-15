39325a63692ae06a295d21dd5292447c4dc8eff6
EZRoot <utohaha@gmail.com>
2025-01-15 18:45:36 -0330

    base

---
+++ Core/MusicApp.cs
@@ -0,0 +1,26 @@
+using SDL2Engine.Core;
+
+namespace MusicPlayer.Core;
+
+public class MusicApp : IGame
+{
+    public void Initialize(IServiceProvider serviceProvider)
+    {
+    }
+
+    public void Update(float deltaTime)
+    {
+    }
+
+    public void Render()
+    {
+    }
+
+    public void RenderGui()
+    {
+    }
+
+    public void Shutdown()
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using SDL2Engine.Core;
+using MusicPlayer.Core;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var app = new GameApp();
+        app.Run(new MusicApp());
+    }
+}
